f2d0731c4813fbea271dc3fb738e3037d5ddd3ed
charlie-harpe <charlie.augusto@outlook.com>
2023-10-12 08:53:18 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Novo gato",
+    "descrption":"Pêlos vibrissas: Os bigodes dos gatos, chamados de vibrissas, são extremamente sensíveis e ajudam os gatos a detectar mudanças sutis no ambiente e na proximidade de objetos.",
+    "photoCover": "https://i.ytimg.com/vi/YSHDBB6id4A/maxresdefault.jpg"
+
+  },
+  {
+    "id":"2",
+    "title":"Novo gato 2",
+    "descrption":"descrição gato 2",
+    "photoCover": "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_4x3.jpg"
+  },
+  {
+    "id":"3",
+    "title":"Novo gato 3",
+    "descrption":"descrição gato 3",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW6mI8xj0v33scsEXwwAq-sKjvnaeiQemgaA&usqp=CAU"
+  },
+  {
+    "id":"4",
+    "title":"Novo gato 4",
+    "descrption":"descrição gato 4",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdvBzlrLIgoP3ADYp_y0C1aVik9Ue1r7stsw&usqp=CAU"
+  }
+];
